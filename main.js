ee262018c121237daf00c8ef84d545aff195448d
Leandro Celle <leangerman13@gmail.com>
2023-05-09 02:41:12 -0300

    Modifico logo, agrego productos y comento en js

--- main.js
+++ main.js
@@ -1,53 +1,4 @@
-/*
-let usuario= prompt(`Ingrese su nombre de usuario`)
-let contrasena= prompt(`Ingrese su contraseña`)
-let edad= prompt(`Ingrese su edad`)
-let bienvenido= alert(`Bienvenido` + " " + usuario + "!")
-
-if (usuario === "leancelle" && contrasena === "12345" && edad >= 18) {
-    alert(`Acceso permitido`)
-}else if (usuario ==="leancelle" && contrasena === "12345" && edad <= 18){
-    alert(`Acceso denegado (no cumple con la edad mínima)`)
-}else {
-    alert(`Acceso denegado`)
-}*/
-
-/*
-let productos= prompt(`
-1- Remera Adidas
-2- Ojotas Kappa
-3- Camperon de Independiente
-4- Camiseta de San Lorenzo
-5- Zapatillas Adidas rojas`)
-
-switch (productos) {
-    case "1":
-        console.log(`Remera Adidas`)
-        break;
-    case "2":
-        console.log(`Ojotas Kappa`)
-        break
-    case "3":
-        console.log(`Camperon de Independiente`)
-        break
-    case "4":
-        console.log(`Camiseta de San Lorenzo`)
-        break
-    case "5":
-        console.log(`Zapatillas Adidas rojas`)
-        break
-    default:
-        console.log(`No se ha encontrado producto para su búsqueda`)
-        break;
-}*/
-/*
-for (let precio = 100; precio <= 200; precio++){
-    if(precio > 150) {
-        break
-    }
-    console.log(`Precio ${precio}`)
-}*/
-
+// JAVASCRIPT
 
 let usuario = prompt("Ingrese su nombre de usuario")
 let contrasena =  prompt("Ingrese su contraseña")
@@ -74,13 +25,13 @@ function productos() {
     let eleccion = prompt("Seleccione el producto que desea comprar: \n 1-Remera Adidas \n 2-Zapatillas Nike \n 3- Botines Puma \n 4- Todos los productos")
 
     if (eleccion === "1") {
-        alert(`Seleccionanste remera Adidas $${remeraAdidas}`)
+        alert(`Agregaste correctamente: Remera Adidas por $${remeraAdidas}`)
     }else if (eleccion === "2"){
-        alert(`Seleccionaste zapatillas Nike $${zapatillasNike}`)
+        alert(`Agregaste correctamente: Zapatillas Nike por $${zapatillasNike}`)
     }else if (eleccion === "3"){
-        alert(`Seleccionaste botines Puma por un precio de $${botinesPuma}`)
+        alert(`Agregaste correctamente: Botines Puma por $${botinesPuma}`)
     }else if (eleccion === "4"){
-        alert(`Seleccionaste todos los productos por un precio de $${remeraAdidas + zapatillasNike + botinesPuma}`)
+        alert(`Agregaste correctamente todos los productos por $${remeraAdidas + zapatillasNike + botinesPuma}`)
     }else {
         console.log(`No seleccionó ningun producto`)
     }
@@ -102,6 +53,24 @@ for (finalizarCompra = "no"; finalizarCompra = "si"; finalizarCompra++) {
 }
 
 alert(`${bienvenido}`)
+
+/*
+
+function RemeraAdidas(nombre, precio, stock) {
+    this.nombre = "Remera Adidas"
+    this.precio = "$8.500"
+    this.stock = "(8 unidades disponibles)"
+    this.alertRemeraAdidas = function(){
+        return alert(`Agregaste correctamente: ${this.nombre} por ${this.precio}. ${this.stock} `)
+    } 
+}
+
+RemeraAdidas()
+const nike = new RemeraAdidas(nombre = "Zapatillas Nike", precio = "25.000", stock = "(5 unidades disponibles")
+const puma = new RemeraAdidas(nombre = "Botines Puma", precio = "22.000", stock = "(10 unidades disponibles")
+alertRemeraAdidas()
+
+*/
 
 
 
