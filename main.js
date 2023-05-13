35794cb18ed0563e6f051ecf4795a925ea74ebd0
Leandro Celle <leangerman13@gmail.com>
2023-05-13 04:15:46 -0300

    Agrego mockup y modifico js

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 // JAVASCRIPT
 
-let usuario = prompt("Ingrese su nombre de usuario")
-let contrasena =  prompt("Ingrese su contraseña")
-let edad = prompt("Ingrese su edad")
-let bienvenido = `Bienvenido ${usuario}! Comienza a buscar tus productos favoritos en FinishLine.`
+const usuario = prompt("Ingrese su nombre de usuario")
+const contrasena =  prompt("Ingrese su contraseña")
+const edad = prompt("Ingrese su edad")
+const bienvenido = `Bienvenido ${usuario}! Comienza a buscar tus productos favoritos en FinishLine.`
 
 function registrarse() {
     if (usuario === "leancelle" && contrasena === "12345" && edad >= 18) {
@@ -17,60 +17,81 @@ function registrarse() {
 
 registrarse()
 
-let remeraAdidas = 8500
-let zapatillasNike = 25000
-let botinesPuma = 22000
+const remeraAdidas = 8500
+const zapatillasNike = 25000
+const botinesPuma = 22000
+
 
 function productos() {
-    let eleccion = prompt("Seleccione el producto que desea comprar: \n 1-Remera Adidas \n 2-Zapatillas Nike \n 3- Botines Puma \n 4- Todos los productos")
-
-    if (eleccion === "1") {
+    const eleccion = prompt("Seleccione el producto que desea comprar: \n 1-Remera Adidas \n 2-Zapatillas Nike \n 3- Botines Puma \n 4- Todos los productos")
+    if (eleccion === 8500 || eleccion === "Remera Adidas" || eleccion === "1") {
         alert(`Agregaste correctamente: Remera Adidas por $${remeraAdidas}`)
-    }else if (eleccion === "2"){
+    }else if (eleccion === 25000 || eleccion === "Zapatillas Nike" || eleccion === "2"){
         alert(`Agregaste correctamente: Zapatillas Nike por $${zapatillasNike}`)
-    }else if (eleccion === "3"){
+    }else if (eleccion === 22000|| eleccion === "Botines Puma" || eleccion === "3"){
         alert(`Agregaste correctamente: Botines Puma por $${botinesPuma}`)
-    }else if (eleccion === "4"){
+    }else if (eleccion=== "Todos los productos" || eleccion === "4"){
         alert(`Agregaste correctamente todos los productos por $${remeraAdidas + zapatillasNike + botinesPuma}`)
     }else {
         console.log(`No seleccionó ningun producto`)
     }
 }
 
-productos()
-
-
-for (finalizarCompra = "no"; finalizarCompra = "si"; finalizarCompra++) {
-    let finalizarCompra = prompt("Desea finalizar su compra? \n SI - NO")
-
-    if (finalizarCompra === "no") {
-        productos()
-    }else if (finalizarCompra === "si") {
-        break
-    }else {
-        break
+function finalCompra() {
+    for (let finalizarCompra = "no"; finalizarCompra = "si"; finalizarCompra++) {
+        let finalizarCompra = prompt("Desea finalizar su compra? \n SI - NO")
+        if (finalizarCompra === "no" || finalizarCompra === "No" || finalizarCompra === "NO") {
+            productos()
+        }else if (finalizarCompra === "si" || finalizarCompra === "Si" || finalizarCompra === "SI") {
+            break
+        }else {
+            break
+        }
     }
 }
 
-alert(`${bienvenido}`)
+productos()
+finalCompra()
 
-/*
 
-function RemeraAdidas(nombre, precio, stock) {
-    this.nombre = "Remera Adidas"
-    this.precio = "$8.500"
-    this.stock = "(8 unidades disponibles)"
-    this.alertRemeraAdidas = function(){
-        return alert(`Agregaste correctamente: ${this.nombre} por ${this.precio}. ${this.stock} `)
+alert(bienvenido)
+
+
+const adidas = [
+    producto0 = "Camperon adidas Argentina Condivo 2022",
+    producto1 = "Camiseta Argentina Campeones Titular",
+    producto2 = "Conjunto Adidas Essential Negro",
+    producto3 = "Botines De Fútbol Adidas Predator Unisex Negro",
+]
+
+console.log(adidas[0])
+console.log(adidas[1])
+console.log(adidas[2])
+console.log(adidas[3])
+adidas.push("Campera Adidas Tiro Pride")
+console.log(adidas[4])
+
+
+function InfoProductos (nombre, precio, stock, marca) {
+    this.nombre = nombre
+    this.precio = precio
+    this.stock = stock
+    this.marcas = marca
+    this.alertInfoProductos = function(){
+        return `Agregaste correctamente: ${this.nombre} ${marca} por $${this.precio}. ${this.stock} `
     } 
 }
 
-RemeraAdidas()
-const nike = new RemeraAdidas(nombre = "Zapatillas Nike", precio = "25.000", stock = "(5 unidades disponibles")
-const puma = new RemeraAdidas(nombre = "Botines Puma", precio = "22.000", stock = "(10 unidades disponibles")
-alertRemeraAdidas()
 
-*/
+
+
+const nike = new InfoProductos("Zapatillas", 25000, "(5 unidades disponibles).", "Nike")
+console.log(nike)
+console.log(nike.alertInfoProductos())
+
+const puma = new InfoProductos("Botines", 22000, "(10 unidades disponibles).", "Puma")
+console.log(puma)
+console.log(puma.alertInfoProductos())
 
 
 
