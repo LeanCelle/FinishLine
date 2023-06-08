ea19c03c028cdb964bb029afe5e221a2db982b08
Leandro Celle <leangerman13@gmail.com>
2023-06-08 03:30:24 -0300

    Modifico js y agrego estilos

--- main.js
+++ main.js
@@ -19,57 +19,62 @@ let productos = [
 
 
 ]
-
+  
 // Carrito
-let carritoCompras = []
-
-// Creo cards
+let carritoCompras = [];
+  
+// Crear tarjeta
 function crearTarjeta(producto, indice) {
     const card = document.createElement("div");
-    card.classList.add("card", "col-sm-12", "col-lg-3");
+    card.className = "card col-sm-12 col-lg-3 divProductos-1";
     card.innerHTML = `
       <img src="${producto.imagen}" class="card-img-top" alt="...">
       <div class="card-body">
-        <h5 class="card-title">${producto.nombre}</h5>
-        <p class="card-text">$${producto.precio}</p>
-        <a href="#cart" class="btn btn-danger" onClick="agregarAlCarrito(${indice})">Agregar al carrito</a>
+        <h5 class="card-title h5Prod">${producto.nombre}</h5>
+        <p class="card-text pProd">$${producto.precio}</p>
+        <a class="btn btn-dark boton" onClick="agregarAlCarrito(${indice})">Agregar al carrito</a>
       </div>
     `;
     return card;
 }
 
-// Filtro
+// Contenedor
+const contenedor = document.getElementById("container");
 
-function adidasFiltro() {
+productos.forEach((producto, indice) => {
+  const card = crearTarjeta(producto, indice);
+  contenedor.appendChild(card);
+});
+
+  
+// Filtrar productos por marca
+function filtrarPorMarca(marca) {
     const contenedor = document.getElementById("container");
     contenedor.innerHTML = "";
-  
-    const filtroMarca = productos.filter((p) => p.marca === "Adidas");
+    
+    const filtroMarca = productos.filter((p) => p.marca === marca);
     filtroMarca.forEach((producto, indice) => {
       const card = crearTarjeta(producto, indice);
       contenedor.appendChild(card);
     });
 }
   
-function nikeFiltro() {
-    const contenedor = document.getElementById("container");
-    contenedor.innerHTML = "";
-  
-    const filtroMarca = productos.filter((p) => p.marca === "Nike");
-    filtroMarca.forEach((producto, indice) => {
-      const card = crearTarjeta(producto, indice);
-      contenedor.appendChild(card);
-    });
-}
-  
-// Agrega eventos de clic a los elementos <h5> de los filtros
+// Agrego eventos de click a los elementos <h5> de los filtros
 const filtroNike = document.getElementById("filtro-nike");
 const filtroAdidas = document.getElementById("filtro-adidas");
-  
-filtroNike.addEventListener("click", nikeFiltro);
-filtroAdidas.addEventListener("click", adidasFiltro);
+const filtroPuma = document.getElementById("filtro-puma");
+const filtroCanterbury = document.getElementById("filtro-canterbury");
+const filtroUnderArmour = document.getElementById("filtro-underArmour");
 
-// Elimino filtros
+  
+filtroNike.addEventListener("click", () => filtrarPorMarca("Nike"));
+filtroAdidas.addEventListener("click", () => filtrarPorMarca("Adidas"));
+filtroPuma.addEventListener("click", () => filtrarPorMarca("Puma"));
+filtroCanterbury.addEventListener("click", () => filtrarPorMarca("Canterbury"));
+filtroUnderArmour.addEventListener("click", () => filtrarPorMarca("Under Armour"));
+
+  
+// Mostrar todos los productos
 function mostrarTodos() {
     const contenedor = document.getElementById("container");
     contenedor.innerHTML = "";
@@ -80,62 +85,60 @@ function mostrarTodos() {
     });
 }
   
-// Agrega evento de clic al elemento <h5> del botón de eliminar filtros
+// Agregar evento de clic al elemento <h5> del botón de eliminar filtros
 const eliminarFiltros = document.getElementById("eliminar-filtros");
 eliminarFiltros.addEventListener("click", mostrarTodos);
   
+// Mostrar todos los productos al cargar la página
+window.addEventListener("load", mostrarTodos);
   
-
-
-// Contenedor
-  
-const contenedor = document.getElementById("container");
-contenedor.style.display = "flex";
-contenedor.style.flexWrap = "wrap";
-contenedor.style.gap = "2rem";
-contenedor.style.justifyContent = "center";
-  
-productos.forEach((producto, indice) => {
-    const card = crearTarjeta(producto, indice);
-    contenedor.appendChild(card);
-});
-
-
-// Precio TOTAL de la compra
+// Actualizar el precio total de la compra
 function actualizarTotal() {
     const totalCompra = carritoCompras.reduce((acc, el) => acc + el.precio * el.cantidad, 0);
     const precioTotal = document.querySelector("#totalCompra");
     precioTotal.textContent = `TOTAL: $${totalCompra}`;
+    precioTotal.style.color = "black"
 }
   
+// Agregar producto al carrito
 function agregarAlCarrito(indice) {
     const producto = productos[indice];
-  
-    // Verificar si el producto ya está en el carrito
     const productoExistente = carritoCompras.find((item) => item.id === producto.id);
   
     if (productoExistente) {
-      // Si el producto ya está en el carrito, incrementar el contador
       productoExistente.cantidad++;
     } else {
-      // Si el producto no está en el carrito, agregarlo con un contador inicial de 1
       producto.cantidad = 1;
       carritoCompras.push(producto);
     }
+
+    Toastify({
+        text: "Producto añadido al carrito",
+        duration: 2000,
+        destination: "",
+        newWindow: true,
+        close: true,
+        gravity: "top", // `top` or `bottom`
+        position: "left", // `left`, `center` or `right`
+        stopOnFocus: true, // Prevents dismissing of toast on hover
+        style: {
+            color: "white",
+            background: "rgba(17, 255, 0, 0.441)",
+            fontSize: "1.5rem",
+        },
+        onClick: function(){} // Callback after click
+      }).showToast();
   
-    // Guardar carrito en el local storage
     guardarCarritoEnLocalStorage();
-  
-    // Actualizar la vista del carrito
     actualizarCarritoEnPantalla();
 }
-
+  
+// Guardar carrito en el almacenamiento local
 function guardarCarritoEnLocalStorage() {
     localStorage.setItem("carrito", JSON.stringify(carritoCompras));
 }
   
-  
-
+// Cargar carrito del almacenamiento local al cargar la página
 window.addEventListener("load", () => {
     const carritoGuardado = localStorage.getItem("carrito");
     if (carritoGuardado) {
@@ -144,15 +147,16 @@ window.addEventListener("load", () => {
     }
 });
   
-
+// Actualizar la vista del carrito
 function actualizarCarritoEnPantalla() {
-    const carrito = document.getElementById("cart");
+    const carrito = document.querySelector(".carritoAgregado");
     carrito.innerHTML = "";
   
     carritoCompras.forEach((producto, index) => {
       const item = document.createElement("div");
+      item.className = "divProdAgregados"
       item.innerHTML = `
-        <img src="${producto.imagen}" class="" alt="..."> - ${producto.nombre} - $${producto.precio} (Cantidad: ${producto.cantidad})
+        <img src="${producto.imagen}" class="imgProdAgregados" alt="...">  ${producto.nombre} - $${producto.precio} (Cantidad: ${producto.cantidad})
         <button class="btn btn-outline-danger btn-sm" onClick="eliminarDelCarrito(${index})">Eliminar</button>
       `;
       carrito.appendChild(item);
@@ -160,26 +164,40 @@ function actualizarCarritoEnPantalla() {
   
     actualizarTotal();
 }
-
   
-  
+// Eliminar producto del carrito
 function eliminarDelCarrito(indice) {
     const producto = carritoCompras[indice];
   
     if (producto.cantidad > 1) {
-      // Si hay más de un producto, decrementar el contador
       producto.cantidad--;
     } else {
-      // Si solo hay un producto, eliminarlo del carrito
       carritoCompras.splice(indice, 1);
     }
   
-    // Guardar carrito en el local storage
     guardarCarritoEnLocalStorage();
-  
-    // Actualizar la vista del carrito
     actualizarCarritoEnPantalla();
 }
+
+// Finalizar Compra
+
+const finalizarCompra = document.querySelector("#finalizarCompra")
+
+finalizarCompra.addEventListener("click", () => {
+    Swal.fire(
+        'Compra finalizada!',
+        'Recibirá su producto dentro de las 72hs hábiles!',
+        'success'
+      )
+})
+
+
+
+//
+
+
+
+  
   
   
 
@@ -249,401 +267,6 @@ adidasFilter.addEventListener("click", () => {
 
     console.log(filtroMarca1);
 })*/ 
-
-
-
-
-for (producto of productos) {
-
-    // SECTION
-    
-const section = document.querySelector("#productos")
-section.style.display = "flex"
-section.style.flexDirection = "column"
-section.style.justifyContent = "center"
-section.style.gap = "10rem"
-section.style.width = "100%"
-section.style.marginBottom = "15rem"
-
-
-
-
-// DIV-1
-
-let divIdOne = document.createElement("div")
-divIdOne.id = "divIdOne"
-divIdOne.style.display = "flex"
-divIdOne.style.flexDirection = "row"
-divIdOne.style.flexWrap = "wrap"
-divIdOne.style.justifyContent = "center"
-divIdOne.style.gap = "5rem"
-divIdOne.style.width = "100%"
-
-// 1
-const divProdOne = document.createElement("div")
-divProdOne.id = "divProdOne"
-divProdOne.style.width = "30rem"
-divProdOne.style.display = "flex"
-divProdOne.style.flexDirection = "column"
-divProdOne.style.padding = "1rem"
-divProdOne.style.boxShadow = "0rem 0rem 0.5rem 0.5rem rgba(0, 0, 0, 0.097)"
-divProdOne.style.justifyContent = "center"
-
-const imgOne = document.createElement("img")
-imgOne.id = "imgIdOne"
-imgOne.src = "../img/camperon-argentina-adidas.jpg"
-imgOne.style.width = "100%"
-
-const h3One = document.createElement("h3")
-let one = productos[0]
-h3One.textContent = one.nombre
-h3One.style.padding = "1rem 0rem 0rem 0rem"
-h3One.style.fontSize = "1.8rem"
-
-const h4One = document.createElement("h4")
-h4One.textContent = "$83.599" 
-h4One.style.padding = "2rem 0rem 0rem 0rem"
-h4One.style.fontSize = "1.2rem"
-
-const btn1 = document.createElement("button")
-btn1.id = "btnId1"
-btn1.textContent = "Agregar al carrito"
-btn1.style.backgroundColor = "black"
-btn1.style.color = "white"
-btn1.style.borderColor = "white"
-btn1.style.margin = "1rem 6rem"
-btn1.style.padding = "1rem"
-btn1.style.fontSize = "1.5rem"
-btn1.style.display = "flex"
-btn1.style.justifyContent = "center"
-btn1.style.alignItems = "center"
-
-
-// 2
-const divProdTwo = document.createElement("div")
-divProdTwo.id = "divProdTwo"
-divProdTwo.style.width = "30rem"
-divProdTwo.style.display = "flex"
-divProdTwo.style.flexDirection = "column"
-divProdTwo.style.padding = "1rem"
-divProdTwo.style.boxShadow = "0rem 0rem 0.5rem 0.5rem rgba(0, 0, 0, 0.097)"
-divProdTwo.style.justifyContent = "center"
-
-const imgTwo = document.createElement("img")
-imgTwo.id = "imgIdTwo"
-imgTwo.src = "../img/camiseta-argentina.jpg"
-imgTwo.style.width = "100%"
-
-const h3Two = document.createElement("h3")
-let two = productos[1]
-h3Two.textContent = two.nombre 
-h3Two.style.padding = "1rem 0rem 0rem 0rem"
-h3Two.style.fontSize = "1.8rem"
-
-const h4Two = document.createElement("h4")
-h4Two.textContent = "$24.490"
-h4Two.style.padding = "2rem 0rem 0rem 0rem"
-h4Two.style.fontSize = "1.2rem"
-
-const btn2 = document.createElement("button")
-btn2.id = "btnId2"
-btn2.textContent = "Agregar al carrito"
-btn2.style.backgroundColor = "black"
-btn2.style.color = "white"
-btn2.style.borderColor = "white"
-btn2.style.margin = "1rem 6rem"
-btn2.style.padding = "1rem"
-btn2.style.fontSize = "1.5rem"
-btn2.style.display = "flex"
-btn2.style.justifyContent = "center"
-btn2.style.alignItems = "center"
-
-// 3
-const divProdThree = document.createElement("div")
-divProdThree.id = "divProdThree"
-divProdThree.style.width = "30rem"
-divProdThree.style.display = "flex"
-divProdThree.style.flexDirection = "column"
-divProdThree.style.padding = "1rem"
-divProdThree.style.boxShadow = "0rem 0rem 0.5rem 0.5rem rgba(0, 0, 0, 0.097)"
-divProdThree.style.justifyContent = "center"
-
-const imgThree = document.createElement("img")
-imgThree.id = "imgIdThree"
-imgThree.src = "../img/campera-adidas.jpg"
-imgThree.style.width = "100%"
-
-const h3Three = document.createElement("h3")
-let three = productos[2]
-h3Three.textContent = three.nombre 
-h3Three.style.padding = "1rem 0rem 0rem 0rem"
-h3Three.style.fontSize = "1.8rem"
-
-const h4Three = document.createElement("h4")
-h4Three.textContent = "$21.299"
-h4Three.style.padding = "2rem 0rem 0rem 0rem"
-h4Three.style.fontSize = "1.2rem"
-
-const btn3 = document.createElement("button")
-btn3.id = "btnId3"
-btn3.textContent = "Agregar al carrito"
-btn3.style.backgroundColor = "black"
-btn3.style.color = "white"
-btn3.style.borderColor = "white"
-btn3.style.margin = "1rem 6rem"
-btn3.style.padding = "1rem"
-btn3.style.fontSize = "1.5rem"
-btn3.style.display = "flex"
-btn3.style.justifyContent = "center"
-btn3.style.alignItems = "center"
-
-// 4
-const divProdFour = document.createElement("div")
-divProdFour.id = "divProdFour"
-divProdFour.style.width = "30rem"
-divProdFour.style.display = "flex"
-divProdFour.style.flexDirection = "column"
-divProdFour.style.padding = "1rem"
-divProdFour.style.boxShadow = "0rem 0rem 0.5rem 0.5rem rgba(0, 0, 0, 0.097)"
-divProdFour.style.justifyContent = "center"
-
-const imgFour = document.createElement("img")
-imgFour.id = "imgIdFour"
-imgFour.src = "../img/botines-violeta.jpg"
-imgFour.style.width = "100%"
-
-const h3Four = document.createElement("h3")
-let four = productos[3]
-h3Four.textContent = four.nombre 
-h3Four.style.padding = "1rem 0rem 0rem 0rem"
-h3Four.style.fontSize = "1.8rem"
-
-
-const h4Four = document.createElement("h4")
-h4Four.textContent = "$39.999" 
-h4Four.style.padding = "2rem 0rem 0rem 0rem"
-h4Four.style.fontSize = "1.2rem"
-
-const btn4 = document.createElement("button")
-btn4.id = "btnId4"
-btn4.textContent = "Agregar al carrito"
-btn4.style.backgroundColor = "black"
-btn4.style.color = "white"
-btn4.style.borderColor = "white"
-btn4.style.margin = "1rem 6rem"
-btn4.style.padding = "1rem"
-btn4.style.fontSize = "1.5rem"
-btn4.style.display = "flex"
-btn4.style.justifyContent = "center"
-btn4.style.alignItems = "center"
-
-// 5
-const divProdFive = document.createElement("div")
-divProdFive.id = "divProdFive"
-divProdFive.style.width = "30rem"
-divProdFive.style.display = "flex"
-divProdFive.style.flexDirection = "column"
-divProdFive.style.padding = "1rem"
-divProdFive.style.boxShadow = "0rem 0rem 0.5rem 0.5rem rgba(0, 0, 0, 0.097)"
-divProdFive.style.justifyContent = "center"
-
-const imgFive = document.createElement("img")
-imgFive.id = "imgIdFive"
-imgFive.src = "../img/short-puma-negro.webp"
-imgFive.style.width = "100%"
-
-const h3Five = document.createElement("h3")
-let five = productos[4]
-h3Five.textContent = five.nombre 
-h3Five.style.padding = "1rem 0rem 0rem 0rem"
-h3Five.style.fontSize = "1.8rem"
-
-
-const h4Five = document.createElement("h4")
-h4Five.textContent = "$5.499" 
-h4Five.style.padding = "2rem 0rem 0rem 0rem"
-h4Five.style.fontSize = "1.2rem"
-
-const btn5 = document.createElement("button")
-btn5.id = "btnId5"
-btn5.textContent = "Agregar al carrito"
-btn5.style.backgroundColor = "black"
-btn5.style.color = "white"
-btn5.style.borderColor = "white"
-btn5.style.margin = "1rem 6rem"
-btn5.style.padding = "1rem"
-btn5.style.fontSize = "1.5rem"
-btn5.style.display = "flex"
-btn5.style.justifyContent = "center"
-btn5.style.alignItems = "center"
-
-// 6
-const divProdSix = document.createElement("div")
-divProdSix.id = "divProdSix"
-divProdSix.style.width = "30rem"
-divProdSix.style.display = "flex"
-divProdSix.style.flexDirection = "column"
-divProdSix.style.padding = "1rem"
-divProdSix.style.boxShadow = "0rem 0rem 0.5rem 0.5rem rgba(0, 0, 0, 0.097)"
-divProdSix.style.justifyContent = "center"
-
-const imgSix = document.createElement("img")
-imgSix.id = "imgIdSix"
-imgSix.src = "../img/campera-adidas-mangas.jpg"
-imgSix.style.width = "100%"
-
-const h3Six = document.createElement("h3")
-let six = productos[5]
-h3Six.textContent = six.nombre 
-h3Six.style.padding = "1rem 0rem 0rem 0rem"
-h3Six.style.fontSize = "1.8rem"
-
-
-const h4Six = document.createElement("h4")
-h4Six.textContent = "$27.999" 
-h4Six.style.padding = "2rem 0rem 0rem 0rem"
-h4Six.style.fontSize = "1.2rem"
-
-const btn6 = document.createElement("button")
-btn6.id = "btnId6"
-btn6.textContent = "Agregar al carrito"
-btn6.style.backgroundColor = "black"
-btn6.style.color = "white"
-btn6.style.borderColor = "white"
-btn6.style.margin = "1rem 6rem"
-btn6.style.padding = "1rem"
-btn6.style.fontSize = "1.5rem"
-btn6.style.display = "flex"
-btn6.style.justifyContent = "center"
-btn6.style.alignItems = "center"
-
-
-// DECLARO 
-
-section.appendChild(divIdOne)
-
-
-divIdOne.appendChild(divProdOne)
-divProdOne.appendChild(imgOne)
-divProdOne.appendChild(h3One)
-divProdOne.appendChild(h4One)
-divProdOne.appendChild(btn1)
-
-
-divIdOne.appendChild(divProdTwo)
-divProdTwo.appendChild(imgTwo)
-divProdTwo.appendChild(h3Two)
-divProdTwo.appendChild(h4Two)
-divProdTwo.appendChild(btn2)
-
-divIdOne.appendChild(divProdThree)
-divProdThree.appendChild(imgThree)
-divProdThree.appendChild(h3Three)
-divProdThree.appendChild(h4Three)
-divProdThree.appendChild(btn3)
-
-divIdOne.appendChild(divProdFour)
-divProdFour.appendChild(imgFour)
-divProdFour.appendChild(h3Four)
-divProdFour.appendChild(h4Four)
-divProdFour.appendChild(btn4)
-
-divIdOne.appendChild(divProdFive)
-divProdFive.appendChild(imgFive)
-divProdFive.appendChild(h3Five)
-divProdFive.appendChild(h4Five)
-divProdFive.appendChild(btn5)
-
-divIdOne.appendChild(divProdSix)
-divProdSix.appendChild(imgSix)
-divProdSix.appendChild(h3Six)
-divProdSix.appendChild(h4Six)
-divProdSix.appendChild(btn6)
-
-
-// EVENTOS BUTTON
-
-const button0 = document.querySelector("#btnId1")
-
-button0.addEventListener("click", () => {
-    carrito.push(productos[0])
-    localStorage.setItem("carrito", JSON.stringify(carrito))
-    console.log(carrito);
-    alert("Has agregado correctamente tu producto al carrito")
-})
-
-
-const button1 = document.querySelector("#btnId2")
-
-button1.addEventListener("click", () => {
-    carrito.push(productos[1])
-    localStorage.setItem("carrito", JSON.stringify(carrito))
-    console.log(carrito);
-    alert("Has agregado correctamente tu producto al carrito")
-})
-
-const button2 = document.querySelector("#btnId3")
-
-button2.addEventListener("click", () => {
-    carrito.push(productos[2])
-    localStorage.setItem("carrito", JSON.stringify(carrito))
-    console.log(carrito);
-    alert("Has agregado correctamente tu producto al carrito")
-})
-
-const button3 = document.querySelector("#btnId4")
-
-button3.addEventListener("click", () => {
-    carrito.push(productos[3])
-    localStorage.setItem("carrito", JSON.stringify(carrito))
-    console.log(carrito);
-    alert("Has agregado correctamente tu producto al carrito")
-})
-
-
-
-// RECUPERO CARRITO
-
-
-const totalEnvio = carrito.reduce((acc, el) => acc + el.precio, 999);
-        
-carrito.forEach(element => {
-    let carrito1 = document.querySelector("#carrito")
-    let divCarrito = document.createElement("div")
-        divCarrito.className = "carritoAgregado"
-        divCarrito.style.display = "flex"
-        divCarrito.style.justifyContent = "center"
-        divCarrito.style.fontSize = "2rem"
-        divCarrito.textContent = "Nombre: " + element.nombre + " Precio: " + element.precio
-        
-    let total = document.querySelector("#total")
-    let h5Total = document.createElement("h5")
-        h5Total.className = "totalCompra"
-        h5Total.textContent = "Total: " + totalCompra
-        
-    total.appendChild(h5Total)
-            
-    carrito1.appendChild(divCarrito)
-
-});
-
-    // FILTER
-
-    const adidas = document.querySelector("#filter")
-    adidas.style.display = "flex"
-
-    const h3Filter = document.createElement("h3")
-    h3Filter.id = "h3Filter"
-    h3Filter.textContent = "Adidas"
-
-    adidas.appendChild(h3Filter)
-
-    adidas.addEventListener("click", () => {
-        divProdFive.style.display = "none"
-    })
-    
-document.body(section)
-}
 
 
 
